a47c560a95d0d5ce0164ee8bea408ba735842adb
deepGIVAn <deepakpvt26@gmail.com>
2024-04-14 19:00:40 +0530

    layout changes

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -13,9 +13,9 @@ export const metadata: Metadata = {
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main className="relative ">
+    <main className="relative">
       <Navbar />
-      <div className="flex ">
+      <div className="flex">
         <Sidebar />
         <section className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14">
           <div className="w-full">{children}</div>
